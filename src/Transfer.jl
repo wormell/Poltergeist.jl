3025841317f91d1aea87ed80fdf3d5ee8b363a86
Caroline Wormell <ca.wormell@gmail.com>
2016-11-09 18:58:30 +1100

    Big speed improvements

--- src/Transfer.jl
+++ src/Transfer.jl
@@ -42,46 +42,39 @@ Base.show(io::IO,L::AbstractTransfer) = print(io,typeof(L)) #temporary
 
 Transfer{T}(::Type{T},m::AbstractMarkovMap) = cache(ConcreteTransfer(T,m),padding=true)
 
-function extendcolstops!(L::ConcreteTransfer,inds,vals) #should be generic range/collection I guess?
+function resizecolstops!(L::ConcreteTransfer,n)
   colstopslength = length(L.colstops)
-  max_ind = maximum(inds)
-  if max_ind > colstopslength
-    append!(L.colstops,-ones(Int,max_ind-colstopslength)) #-1 is clunky but w/e
+  if n > colstopslength
+    append!(L.colstops,-ones(Int,n-colstopslength)) #-1 is clunky but w/e
   end
+end
 
+function setcolstops!(L::ConcreteTransfer,inds,vals) #should be generic range/collection I guess?
+  resizecolstops!(L,maximum(inds))
   L.colstops[inds] = vals
 end
 
-function transferfunction{TT,D}(x,L::ConcreteTransfer{TT,Chebyshev{D}},kk::Integer,T)
+chebyTk(x,d,k::Integer) = cos((k-1)*acos(tocanonical(d,x))) #roundoff error grows linearly(??) with k may not be bad wrt x too
+function transferfunction{TT,D}(x,L::ConcreteTransfer{TT,Chebyshev{D}},k::Integer,T)
   y = zero(x);
-  fn(xx) = cos(kk*acos(tocanonical(domainspace(L),xx))) #this probably introduces lots of roundoff error??? oh well
 
-  for i = 1:Base.length(L.m)
-    y += getmap(L)[:dvdx][i](x).*fn(getmap(L)[:v][i](x))
+  for i = 1:length(L.m)
+    y += getmap(L)[:dvdx][i](x).*chebyTk(getmap(L)[:v][i](x),domain(L),k)
   end;
-  abs(y) < 200eps(one(y)) ? zero(y) : y
+  abs(y) < 20k*eps(one(abs(y))) ? zero(y) : y
 end
 
-function transferfunction{TT,D}(x,L::ConcreteTransfer{TT,Fourier{D}},kk::Integer,T)
+fourierCSk(x,d,k::Integer) = rem(k,2) == 1 ? cos(fld(k,2)*tocanonical(d,x)) : sin(fld(k,2)*tocanonical(d,x))
+function transferfunction{TT,D}(x,L::ConcreteTransfer{TT,Fourier{D}},k::Integer,T)
   y = zero(x);
-  if rem(kk,2) == 1
-    fnh = cos
-  else
-    fnh = sin
-  end
-
-  #     if rem(kk,2) == 1
-  #         fn(xx) = cos(div(kk-1,2)*tocanonical(domainspace(L),xx))
-  #     else
-  #         fn(xx) = sin(div(kk,2)*tocanonical(domainspace(L),xx))
-  #     end
 
   for i = 1:Base.length(L.m)
-    y += getmap(L)[:dvdx][i](x).*fnh(fld(kk,2)*tocanonical(domainspace(L),getmap(L)[:v][i](x)))
+    y += getmap(L)[:dvdx][i](x).*fourierCSk(getmap(L)[:v][i](x),domain(L),k)
   end;
-  abs(y) < 20kk*eps(one(y)) ? zero(y) : y
+  abs(y) < 20k*eps(one(abs(y))) ? zero(y) : y
 
 end
+
 
 function default_transferfunction{TT,DD}(x,L::ConcreteTransfer{TT,DD},kk::Integer,T)
   y = zero(x);
@@ -90,7 +83,7 @@ function default_transferfunction{TT,DD}(x,L::ConcreteTransfer{TT,DD},kk::Intege
   for i = 1:Base.length(L.m)
     y += getmap(L)[:dvdx][i](x).*fn(getmap(L)[:v][i](x))
   end;
-  abs(y) < 200eps(one(y)) ? zero(y) : y
+  abs(y) < 200eps(one(abs(y))) ? zero(y) : y
 end
 transferfunction(x,L,kk,T) = default_transferfunction(x,L,kk,T)
 
@@ -102,25 +95,80 @@ function transfer_getindex{T,D,R,M<:InverseDerivativeMarkovMap}(L::ConcreteTrans
   cols = Array(eltype(k),Base.length(k)+1)
   cols[1] = 1
   K = isfinite(jdat[3]) ? length(jdat[1]:jdat[2]:jdat[3]) : 0 # largest colstop
-  colstops = Array(Int,length(k))
+
+  rs = rangespace(L)
+
+  resizecolstops!(L,maximum(k))
+  mc = maximum(L.colstops[1:first(k)])
 
   for (kind,kk) in enumerate(k)
-    tk = Fun(x->transferfunction(x,L,kk,T),rangespace(L))
-    tol = 4ceil(log2(length(tk.coefficients)))*eps(T) # 4 is dummy should be like 1 in Fourier, 2C+1 in Cheby
-    chop!(tk,tol)
+    kind > 1 && (mc = max(mc,maximum(L.colstops[k[kind-1]+1:kk])))
 
-    ltkc = length(tk.coefficients)
-    for i = 1:ltkc
-      abs(tk.coefficients[i])<tol && (tk.coefficients[i] = 0)
+    f(x) = transferfunction(x,L,kk,T)
+    tol =T==Any?200eps():200eps(T)
+
+    if L.colstops[kk] >= 1
+      cf = Fun(f,rs,convert(Int,2^max(4,ceil(log2(L.colstops[kk])))))
+    elseif L.colstops[kk]  == 0
+      cf = zeros(rs)
+
+    else
+
+      if mc ≤ 2^4
+        cf = Fun(f,rs)
+      else
+        # code from ApproxFun (src/Fun/constructors.jl) - the difference is we start at n \approx mc
+
+        r=ApproxFun.checkpoints(rs)
+        fr=map(f,r)
+        maxabsfr=norm(fr,Inf)
+
+        logn = min(convert(Int,ceil(log2(mc))),20)
+        while logn < 21
+          cf = Fun(f,rs,2^logn)
+
+          maxabsc = maxabs(cf.coefficients)
+          if maxabsc == 0 && maxabsfr == 0
+            cf = (zeros(rs))
+            break
+          else
+            b = ApproxFun.block(rs,length(cf.coefficients))
+            bs = ApproxFun.blockstart(rs,max(b-2,1))
+            if ncoefficients(cf) > 8 && maxabs(cf.coefficients[bs:end]) < tol*maxabsc &&
+                all(kkk->norm(cf(r[kkk])-fr[kkk],1)<tol*length(cf.coefficients)*maxabsfr*1000,1:length(r))
+              cf =  chop!(cf,tol*maxabsc/10)
+              break
+            end
+          end
+          logn += 1
+        end
+
+        if logn == 21
+          warn("Maximum number of coefficients "*string(2^20+1)*" reached in constructing Fun.")
+          Fun(f,rs,2^21)
+        end
+
+      end
     end
 
-    tkc = tk.coefficients[(jdat[1]:jdat[2]:min(ltkc,jdat[3]))::Range]
-    append!(dat,tkc)
-    cols[kind+1] = cols[kind]+length(tkc)
-    K = max(K,length(tkc))
-    colstops[kind] = ltkc
+    #    tol = 4ceil(log2(length(tk.coefficients)))*eps(T) # 4 is dummy should be like 1 in Fourier, 2C+1 in Cheby
+    #    chop!(tk,tol)
+    maxabsc = maxabs(cf.coefficients)
+    lcfc = length(cf.coefficients)
+
+    for i = 1:lcfc
+      abs(cf.coefficients[i])<tol*maxabsc/10 && (cf.coefficients[i] = 0)
+    end
+
+    cutcfc = cf.coefficients[(jdat[1]:jdat[2]:min(lcfc,jdat[3]))::Range]
+
+    append!(dat,cutcfc)
+    cols[kind+1] = cols[kind]+length(cutcfc)
+    K = max(K,length(cutcfc))
+
+    setcolstops!(L,kk,lcfc)
+ #   kk == 1 && display(stacktrace())
   end
-  extendcolstops!(L,k,colstops)
   RaggedMatrix(dat,cols,K)
 end
 
@@ -131,7 +179,15 @@ Base.getindex(L::ConcreteTransfer,j::Integer,k::Integer) = Base.getindex(L,j:j,k
 Base.getindex(L::ConcreteTransfer,j::Integer,k::Range) = Base.getindex(L,j:j,k).data
 Base.getindex(L::ConcreteTransfer,j::Range,k::Integer) = Base.getindex(L,j,k:k).data
 
+function ApproxFun.default_raggedmatrix{T,LL<:AbstractTransfer,R1<:Union{Range,ApproxFun.AbstractCount},R2<:Range}(
+  S::ApproxFun.SubOperator{T,LL,Tuple{R1,R2}})
+ Base.getindex(parent(S),parentindexes(S)[1],parentindexes(S)[2])
+end
+
+
+
 function ApproxFun.colstop(L::ConcreteTransfer,k::Integer)
   k <= length(L.colstops) && L.colstops[k] != -1 && return L.colstops[k]
-  length(transfer_getindex(L,(1,1,ApproxFun.∞),k:k).data)
+  transfer_getindex(L,(1,1,ApproxFun.∞),k:k)
+  L.colstops[k]
 end
--- src/general.jl
+++ src/general.jl
@@ -1,11 +1,11 @@
 rangedomain(op::Operator) = domain(rangespace(op))
 
 containstransfer(M::Operator) = ApproxFun.iswrapper(M::Operator) && containstransfer(M.op)
-for optype in (:(ApproxFun.InterlaceOperator),:(ApproxFun.PlusOperator),:(ApproxFun.MultiplicationOperator))
+for optype in (:(ApproxFun.InterlaceOperator),:(ApproxFun.PlusOperator),:(ApproxFun.TimesOperator))
   eval(:(containstransfer(M::$optype) = any(containstransfer,M.ops)))
 end
 
-
+ApproxFun.colstop(L::ApproxFun.LowRankPertOperator,k::Integer) = max(colstop(L.op,k::Integer),colstop(L.pert,k::Integer))
 
 #Base.isless(a::ApproxFun.Infinity{Bool},b::ApproxFun.Infinity{Bool}) = a.angle && ~b.angle
 
