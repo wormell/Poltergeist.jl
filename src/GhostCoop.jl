772eebaa2c835bd8e79847342fec3b72b6c4fa8e
Caroline Wormell <ca.wormell@gmail.com>
2016-11-18 18:44:53 +1100

    Lots of nice additions

--- src/GhostCoop.jl
+++ src/GhostCoop.jl
@@ -1,13 +1,37 @@
 module GhostCoop
-  using Base, ApproxFun#, Compat, FastTransforms
+  using Base, ApproxFun, BandedMatrices, Compat#, FastTransforms
 
 import Base: values,getindex,setindex!,*,.*,+,.+,-,.-,==,<,<=,>,|,
->=,./,/,.^,^,\,∪,transpose, size, length, issymmetric#, maximum, minimum
-import ApproxFun: domainspace, rangespace, israggedbelow, RaggedMatrix, resizedata!, colstop, CachedOperator, Infinity,
+>=,./,/,.^,^,\,∪,transpose, size, length, issymmetric, eltype#, maximum, minimum
+import ApproxFun: domainspace, rangespace, domain, israggedbelow, RaggedMatrix, resizedata!, colstop, CachedOperator, Infinity,
 fromcanonicalD, tocanonicalD, default_raggedmatrix
 
 include("general.jl")
 include("MarkovMap.jl")
 include("Transfer.jl")
 
+type SchurInvWrapper{QR<:ApproxFun.QROperator,F<:Fun,T} <: Operator{T}
+  op::QR
+  u::F
+end
+SchurInvWrapper(op::ApproxFun.QROperator,f::Fun) = SchurInvWrapper{typeof(op),typeof(f),eltype(op)}(op,f)
+ApproxFun.qrfact!(op::SchurInvWrapper) = op
+ApproxFun.qrfact(op::SchurInvWrapper) = op
+#SchurInvWrapper(op::Operator) = SchurInvWrapper(op,uniform(domainspace(op)))
+ApproxFun.@wrapper SchurInvWrapper
+ApproxFun.linsolve(L::SchurInvWrapper,b;kwds...) = linsolve(L.op,b;kwds...)
+
+uniform(S::Space) = Fun(1.,S)/sum(Fun(1.,S))
+uniform(D::Domain) = Fun(1.,D)/sum(Fun(1.,D))
+function SchurInv(L::Operator,u::Fun=uniform(domainspace(L)))
+  @assert domain(L) == rangedomain(L)
+  SchurInvWrapper(qrfact(I-L + cache(u/sum(u) * DefiniteIntegral(domainspace(L)))),u)
+end
+
+acim(L::Operator,u::Fun=uniform(domainspace(L))) = SchurInv(L,u)\u
+acim(S::SchurInvWrapper) = S.op\S.u
+linearresponse(S::SchurInvWrapper,X::Fun) = S\(-acim(S)*X)'
+linearresponse(L::Operator,X::Fun) = linearresponse(SchurInv(L),X)
+correlationsum(S::SchurInvWrapper,A::Fun) = S\(acim(S)*A)
+correlationsum(L::Operator,A::Fun) = correlationsum(SchurInv(L),A)
 end # module
--- src/MarkovMap.jl
+++ src/MarkovMap.jl
@@ -1,7 +1,7 @@
 export domain, rangedomain
 
 abstract AbstractMarkovMap{D<:Domain,R<:Domain,T,FF} <: Function
-abstract AbstractDerivativeMarkovMap{D<:Domain,R<:Domain,T,FF} <: AbstractMarkovMap{D,R,T,FF}
+#abstract AbstractDerivativeMarkovMap{D<:Domain,R<:Domain,T,FF} <: AbstractMarkovMap{D,R,T,FF}
 
 Base.summary(m::AbstractMarkovMap) =  string(typeof(m).name.name)*":"*string(domain(m))*"↦"*string(rangedomain(m)) #branches??
 Base.eltype(m::AbstractMarkovMap) = eltype(rangedomain(m))
@@ -9,90 +9,182 @@ Base.show(io::IO,m::AbstractMarkovMap) = print(io,typeof(m)) #temporary
 
 
 immutable MarkovMap{D<:Domain,R<:Domain,T,ff} <: AbstractMarkovMap{D,R,T,ff}
-    domain::D
-    range::R
-    f::ff
-    bl::Vector{T} #lower edges of branches
-    bu::Vector{T} #upper edges of branches
-    sgns::Vector{T} #sign of derivative of f on branches
-#     λhat::T #maximum derivative
-#     C1::T #first distortion bound
-    # cache of inverse branches of f
-    function FunctionMarkovMap{T}(
-        dom::Domain,ran::Domain,f,bl::Vector{T},bu::Vector{T},sgns::Vector{T})
-        @assert length(bl) == length(bu)
-        @assert length(bl) == length(sgns)
-        @assert all(sgns.^2 == 1)
-        @assert all(bl.<bu)
-        @assert sum(bu-bl) <= arclength(dom)
-            # assert branches non-overlapping, in D,...
-        new{typeof(dom),typeof(ran),T,typeof(f)}(dom,ran,f,bl,bu,sgns)
-    end
+  domain::D
+  rangedomain::R
+  f::Vector{ff}
+  dfdx::Vector{ff}
+  bl::Vector{T} #lower edges of branches
+  bu::Vector{T} #upper edges of branches
+  sgns::Vector{T} #sign of derivative of f on branches
+  #     λhat::T #maximum derivative
+  #     C1::T #first distortion bound
+  # cache of inverse branches of f
+
+end
+# MarkovMap{T,ff<:Number}(dom::Domain,ran::Domain,f::Vector{ff},dfdx::Vector{ff},bl::Vector{T},bu::Vector{T}) =
+#   MarkovMap{T,ff}(dom,ran,f,dfdx,bl,bu)
+
+function MarkovMap{D,R,T,ff}(
+    dom::D,ran::R,f::Vector{ff},dfdx::Vector{ff},bl::Vector{T},bu::Vector{T})
+  @assert length(bl) == length(bu)
+  @assert length(f) == length(bl)
+  @assert length(dfdx) == length(bl)
+  #        @assert length(bl) == length(sgns)
+  #        @assert all(sgns.^2 == 1)
+  @assert all([in(bll,dom) for bll in bl])
+  @assert all([in(buu,dom) for buu in bu])
+  @assert all(bl.<bu)
+  bl_sortperm = sortperm(bl)
+  @assert all(bu[bl_sortperm[1:end-1]] .<= bl[bl_sortperm[2:end]])
+  #     if isa(ran,ApproxFun.IntervalDomain)
+  #       @assert all([in(f[i](bl[i]),∂(ran)) for i = 1:length(bl)])
+  #       @assert all([in(f[i](bu[i]),∂(ran)) for i = 1:length(bu)])
+  #     end
+  sgns = T[sign(f[i](bu[i])-f[i](bl[i])) for i = 1:length(bl)]
+  MarkovMap(dom,ran,f,dfdx,bl,bu,sgns)
 end
 
+function MarkovMap{D,R,ff}(
+    dom::D,ran::R,f::Vector{ff},dfdx::Vector{ff})
+  T = eltype(R)
+  bl = Array(T,length(f)); bu = Array(T,length(f))
+  tol = 10eps(maxabs([dom.a,dom.b]))
+  for i = 1:length(f)
+    ba = interval_newton(f[i],dfdx[i],ran.a,dom.a,dom.b,tol)
+    bb = interval_newton(f[i],dfdx[i],ran.b,dom.a,dom.b,tol)
+    bl[i] = min(ba,bb); bu[i] = max(ba,bb)
+    bl[i] < dom.a && bl[i]-dom.a > -2.5tol && (bl[i] = dom.a)
+    bu[i] > dom.b && bu[i]-dom.b < 2.5tol && (bu[i] = dom.b)
+  end
+  for i = 1:length(f)
+    for j = 1:length(f)
+      abs(bu[j]-bl[i]) < 2.5tol && ((j ==i) ?
+        error("Width of branch $i too small for automated branch edge checking...") :
+        (bu[j] = bl[i] = (bu[j]+bl[i])/2))
+    end
+  end
+  MarkovMap(dom,ran,f,dfdx,bl,bu)
+end
+#MarkovMap(dom::D,ran::R,f::Vector{ff},dfdx::Vector{ff}) = MarkovMap{D,R,eltype(R),ff}(dom,ran,f,dfdx)
+MarkovMap{ff<:ApproxFun.Fun}(dom,ran,f::Vector{ff}) = MarkovMap(dom,ran,f,[fi' for fi in f])
+
+# function MarkovMap{ff,gg}(dom::Domain,ran::Domain,f::Vector{ff},dfdx::Vector{gg},args...)
+#   pp = promote_type(ff,gg)
+#   MarkovMap(dom,ran,convert(Vector{pp},f),convert(Vector{pp},dfdx),args...)
+# end
+MarkovMap{ff}(dom::Domain,f::Vector{ff},args...) = MarkovMap(dom,dom,f,args...)
+
 length(m::MarkovMap) = length(m.bl)
+branch(m::MarkovMap,x::Number) = findfirst([x .<= bu[i] && x .>= bl[i] for i=1:length(bu)])
+(m::MarkovMap)(i::Integer,x) = m.f[i](x)
+(m::MarkovMap)(x::Number) = m.f[branch(m,x)](x)
+markovD(m::MarkovMap,i::Integer,x) = m.dfdx[i](x)
+markovD(m::MarkovMap,x::Number) = m.dfdx[branch(m,x)](x)
+markovinv(m::MarkovMap,i::Integer,x::Number) = interval_newton(m.f[i],m.dfdx[i],x,m.bl[i],m.bu[i])
+markovinvD(m::MarkovMap,i::Integer,x::Number) = 1/m.dfdx[i](markovinv(m,i,x))
+markovDsign(m::MarkovMap,i::Integer) = m.sgns[i]
 
+immutable InverseDerivativeMarkovMap{D<:Domain,R<:Domain,T,FF} <: AbstractMarkovMap{D,R,T,FF}
+  domain::D
+  rangedomain::R
+  v::Vector{FF} #branches of inverse
+  dvdx::Vector{FF} #derivatives of branches of inverse
+  sgns::Vector{T} #sign of derivative of f on branches
 
-immutable InverseDerivativeMarkovMap{D<:Domain,R<:Domain,T,FF} <: AbstractDerivativeMarkovMap{D,R,T,FF}
-    domain::D
-    rangedomain::R
-    v::Vector{FF} #branches of inverse
-    dvdx::Vector{FF} #derivatives of branches of inverse
-    sgns::Vector{T} #sign of derivative of f on branches
-#     λhat::T #maximum derivative
-#     C1::T #first distortion bound
-#    InverseDerivativeMarkovMap(v,dvdx,sgns,Ds,Rs,T,FF) = new(v,)
-
-    # assert stuff??
- #   function InverseDerivativeMarkovMap{FF,T}(dom::Domain,ran::Domain,v::Vector{FF},dvdx::Vector{FF},sgns::Vector{T})
- #       @assert length(v) == length(dvdx)
- #       @assert length(v) == length(sgns)
- #       @assert all(sgns.^2 == 1)
- #       new(dom,ran,v,dvdx,sgns)
- #   end
+  # assert stuff??
+  #   function InverseDerivativeMarkovMap{FF,T}(dom::Domain,ran::Domain,v::Vector{FF},dvdx::Vector{FF},sgns::Vector{T})
+  #       @assert length(v) == length(dvdx)
+  #       @assert length(v) == length(sgns)
+  #       @assert all(sgns.^2 == 1)
+  #       new(dom,ran,v,dvdx,sgns)
+  #   end
 end
 
 function InverseDerivativeMarkovMap{FF}(dom::Domain,ran::Domain,v::Vector{FF},dvdx::Vector{FF})
-    sgnpt = rand(ran); #hack
-    return InverseDerivativeMarkovMap(dom,ran,v,dvdx,eltype(dom)[sign(dvdxi(sgnpt)) for dvdxi in dvdx])
+  sgnpt = rand(ran); #hack
+  return InverseDerivativeMarkovMap(dom,ran,v,dvdx,eltype(dom)[sign(dvdxi(sgnpt)) for dvdxi in dvdx])
 end
+InverseDerivativeMarkovMap{FF}(dom::Domain,v::Vector{FF},dvdx::Vector{FF}) =
+  InverseDerivativeMarkovMap(dom,dom,v,dvdx)
+
 
 length(m::InverseDerivativeMarkovMap) = length(m.v)
-function getindex(m::InverseDerivativeMarkovMap,d::Symbol)
-    d==:v && return m.v
-    d==:dvdx && return m.dvdx
+markovinv(m::InverseDerivativeMarkovMap,i::Integer,x) = m.v[i](x)
+markovinvD(m::InverseDerivativeMarkovMap,i::Integer,x) = m.dvdx[i](x)
+markovDsign(m::InverseDerivativeMarkovMap,i::Integer) = m.sgns[i]
 
-    error("Symbol not recognised")
-end
 #getindex(m::InverseDerivativeMarkovMap,:dvdx) = m.dvdx
 
 for TYP in (:MarkovMap,:InverseDerivativeMarkovMap)
-    @eval ApproxFun.domain(m::$TYP) = m.domain
-    @eval rangedomain(m::$TYP) = m.rangedomain
+  @eval ApproxFun.domain(m::$TYP) = m.domain
+  @eval rangedomain(m::$TYP) = m.rangedomain
 end
 
-abstract MarkovRegularityWrapper{D<:Domain,R<:Domain,T,FF} <: AbstractMarkovMap{D,R,T,FF}
-
-type AnalyticMarkovRegularityWrapper{D<:Domain,R<:Domain,T,FF} <: MarkovRegularityWrapper{D,R,T,FF}
-  mm::AbstractMarkovMap{D,R,T,FF}
-  delta::T
-  p::T
-  K::T
+type MarkovInverseCache{M<:AbstractMarkovMap,S<:Space,T} <: AbstractMarkovMap
+  m::M
+  sp::S
+  cache::Dict{Int,Array{T,2}}
+end
+function MarkovInverseCache(m::AbstractMarkovMap,s::Space)
+  @assert rangedomain(m) == domain(s)
+  MarkovInverseCache{typeof(m),typeof(s),eltype(domain(m))}(m,s,Dict{Int,Array{eltype(domain(m)),2}}())
 end
 
-function getanalyticp{D<:Domain,R<:PeriodicInterval,T,FF}(mm::AbstractDerivativeMarkovMap{D,R,T,FF},delta::T)
-  try
-    maxabs(Fun(x->imag(mm[:dvdx](x+im*delta)),space(R)))
-  catch
-    mmf = Fun(mm[:dvdx],space(R))
-    maxabs(Fun(x->imag(mmf(x+im*delta)),space(R)))
+MarkovInverseCache(m::AbstractMarkovMap) = MarkovInverseCache(m,ApproxFun.Space(rangedomain(m)))
+
+ApproxFun.domain(mc::MarkovInverseCache) = domain(mc.m)
+rangedomain(mc::MarkovInverseCache) = rangedomain(mc.m)
+length(mc::MarkovInverseCache) = length(mc.m)
+rangespace(mc::MarkovInverseCache) = mc.sp
+function extenddata!(m::MarkovInverseCache,n::Int)
+  v = Array(eltype(m),length(m),n)
+  pts = points(rangespace(m),n)
+  for k = 1:n
+    for i = 1:length(m)
+      v[i,k] = markovinv(m.m,i,pts[k])
+    end
   end
+  push!(m.cache,n=>v)
 end
 
-
-type DifferentiableMarkovRegularityWrapper{D<:Domain,R<:Domain,T,FF} <: MarkovRegularityWrapper{D,R,T,FF}
-  mm::AbstractMarkovMap{D,R,T,FF}
-  r::Int
-  W::Array{T}
+for FN in (:markovinv,:markovinvD)
+  @eval $FN(m::MarkovInverseCache,i::Integer,x::Number) = $FN(m.m,i,x)
 end
+
+function markovinv(m::MarkovInverseCache,i::Integer,x::InterpolationNode)
+  rangespace(m) != space(x) && return markovinv(m,i,convert(eltype(x),x))
+  ~haskey(m.cache,x.n) && extenddata!(m,x.n)
+  m.cache[x.n][i,x.k]
+end
+markovinvD{M<:InverseDerivativeMarkovMap}(m::MarkovInverseCache{M},i::Integer,x::InterpolationNode) =
+  markovinvD(m.m,i,x)
+markovinvD{M<:MarkovMap}(m::MarkovInverseCache{M},i::Integer,x::InterpolationNode) =
+  1/markovD(m.m,i,markovinv(m,i,x))
+markovDsign(m::MarkovInverseCache,i::Integer) = markovDsign(m.m,i)
+
+
+# abstract MarkovRegularityWrapper{D<:Domain,R<:Domain,T,FF} <: AbstractMarkovMap{D,R,T,FF}
+
+# type AnalyticMarkovRegularityWrapper{D<:Domain,R<:Domain,T,FF} <: MarkovRegularityWrapper{D,R,T,FF}
+#   mm::AbstractMarkovMap{D,R,T,FF}
+#   delta::T
+#   p::T
+#   K::T
+# end
+
+# function getanalyticp{D<:Domain,R<:PeriodicInterval,T,FF}(mm::AbstractDerivativeMarkovMap{D,R,T,FF},delta::T)
+#   try
+#     maxabs(Fun(x->imag(mm[:dvdx](x+im*delta)),space(R)))
+#   catch
+#     mmf = Fun(mm[:dvdx],space(R))
+#     maxabs(Fun(x->imag(mmf(x+im*delta)),space(R)))
+#   end
+# end
+
+
+# type DifferentiableMarkovRegularityWrapper{D<:Domain,R<:Domain,T,FF} <: MarkovRegularityWrapper{D,R,T,FF}
+#   mm::AbstractMarkovMap{D,R,T,FF}
+#   r::Int
+#   W::Array{T}
+# end
 
